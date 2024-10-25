546309aa0a4f22b523b94da62f0ac2c8ac10e09b
paulusminus <info@paulmin.nl>
2024-10-25 14:50:00 +0200

    create command DockerRunSelectedImage

--- plugin/dockerClient.lua
+++ plugin/dockerClient.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_create_user_command("CargoDocTest", require("dockerClient").run_cargo_doctest(), {})
+vim.api.nvim_create_user_command("DockerRunSelectedImage", require("dockerClient").select_docker_image(), {})
