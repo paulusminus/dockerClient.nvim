28e010bdce94ae0b59de6bff4ffa64aaaf074d50
paulusminus <info@paulmin.nl>
2024-10-25 16:49:47 +0200

    update

--- lua/dockerClient/image.lua
+++ lua/dockerClient/image.lua
@@ -1,4 +1,3 @@
-local actions = require("telescope.actions")
 local actions_state = require("telescope.actions.state")
 local plenary = require("plenary")
 local previewers = require("telescope.previewers")
--- plugin/dockerClient.lua
+++ plugin/dockerClient.lua
@@ -1,2 +1,7 @@
+if 1 ~= vim.fn.has("nvim-0.9.0") then
+	vim.api.nvim_err_writeln("Telescope.nvim requires at least nvim-0.9.0.")
+	return
+end
+
 vim.api.nvim_create_user_command("CargoDocTest", require("dockerClient").run_cargo_doctest(), {})
 vim.api.nvim_create_user_command("DockerRunSelectedImage", require("dockerClient").select_docker_image(), {})
