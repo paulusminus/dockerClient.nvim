96a6565c4fdb6ca7765de3a8dae24e294356828b
paulusminus <info@paulmin.nl>
2024-10-29 21:10:01 +0100

    chore: use wait

--- lua/dockerClient/image.lua
+++ lua/dockerClient/image.lua
@@ -23,7 +23,7 @@ local M = {}
 
 M.list_fn = function()
 	-- return plenary.job:new(list_images_command):sync()
-	return vim.system(list_images_command)
+	return vim.system(list_images_command):wait()
 end
 
 M.entry_maker = function(entry)
